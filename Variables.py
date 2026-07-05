cd1aa8567a85b6d657c0671f4b83a4a91cecbc4c
kalpd <kalpdoshi999@gmail.com>
2026-07-05 17:01:45 +0530

    Learnt Variables

---
+++ Variables.py
@@ -0,0 +1,51 @@
+rent = 1220
+gas = 206.5
+groceries = 307.3
+
+print(rent)
+
+total = rent + gas + groceries
+
+print(total)
+
+rent = 1400
+
+item1 = "rent"
+item2 = "gas"
+item3 = "groceries"
+
+print("Expense items:", item1, item2, item3)
+
+
+# Exercise: Python Variables
+#
+#     1. Create a variable called break and assign it a value 5. See what happens and find out the reason behind the behavior that you see.
+#
+#     2. Create two variables. One to store your birth year and another one to store current year. Now calculate your age using these two variables
+#
+#     3. Store your first, middle and last name in three different variables and then print your full name using these variables
+#
+#     4. Answer which of these are invalid variable names: _nation 1record record1 record_one record-one record^one continue
+
+# Kalp Doshi's Solution
+
+# break = 5 # SyntaxError: invalid syntax, Because it is a Keyword.
+# birth_year = 2004
+# current_year = 2026
+#
+# age = current_year - birth_year
+# print(age)
+
+# first_name = "Kalp"
+# middle_name = "Nishitkumar"
+# lastname = "Doshi"
+#
+# print("My full name is", first_name, middle_name, lastname)
+
+# _nation = 1 #Valid
+# 1record = 1 # Invalid
+# record1 = 1 #Valid
+# record_one = 1 #Valid
+# record-one = 1 # Invalid
+# record^one = 1 #Invalid
+# continue = 1 #Invalid
