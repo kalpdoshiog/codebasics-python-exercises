35882916a07f18676f2fdd514046d5597e3578c8
kalpd <kalpdoshi999@gmail.com>
2026-07-08 10:09:16 +0530

    Learning Strings

---
+++ 3 Strings.py
@@ -0,0 +1,15 @@
+text = "Ice Cream"
+
+print(text)
+
+print(text[0]) # Index Zero, Python strings starts with zero (0).
+
+print(text[1]) # It will print a char from index 1 of text variable.
+
+print(text[0:3]) # 0 is included but 3 is not means n-1 (3-1) = 2 so it will go upto 2 only
+
+print(text[4:]) # It will go till end
+
+print(text[:9]) # It will start from zero and go till 8 as (9-1) = 8
+
+# Strings can define by both "" and ''
