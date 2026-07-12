3a71a91e61056609500364779004d80171709b4c
kalpd <kalpdoshi999@gmail.com>
2026-07-12 19:35:31 +0530

    Learning For Loops

---
+++ 6 For Loops.py
@@ -0,0 +1,32 @@
+# expense = [2350, 2100, 3450, 1679, 5321]
+#
+# total = 0
+# for item in expense:
+#     total += item
+#
+# print(total)
+
+
+# for number in range(1, 11):
+#     print(number)
+#
+# for number in range(1, 11):
+#     print(number*number)
+#
+#
+# expense = [2350, 2100, 3450, 1679, 5321]
+# total = 0
+# for item in range(len(expense)):
+#     print(f"Month :", (item+1), "Expense :" , expense[item])
+#     total += expense[item]
+# print(f"Total expense is : {total}")
+
+key_location = "chair"
+locations = ["garage", "living room", "kitchen", "chair", "bedroom", "bath room"]
+
+for key in locations:
+    if key==key_location:
+        print(f"Key is found : {key}")
+        break
+    else:
+        print(f"Key is not found : {key}")
