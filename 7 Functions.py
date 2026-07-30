23894b5cd895e365a06172f267bf1581ab27db18
kalpd <kalpdoshi999@gmail.com>
2026-07-30 09:46:47 +0530

    Learning Functions

--- 7 Functions.py
+++ 7 Functions.py
@@ -36,22 +36,61 @@
 # print(f"Joe's total expense is : {joe_expense}")
 
 
-def sum(a, b=0):
-    """
-    :param a:
-    :param b:
-    :return:
-    """
-    print(f"A : {a}")
-    print(f"B : {b}")
-    total = a + b
-    print(f"Total inside function : {total}")
-    return total
+# def sum(a, b=0):
+#     """
+#     :param a:
+#     :param b:
+#     :return:
+#     """
+#     print(f"A : {a}")
+#     print(f"B : {b}")
+#     total = a + b
+#     print(f"Total inside function : {total}")
+#     return total
+#
+# number = sum(5,6)
+# number_2 = sum(b=5, a=6)
+# number_3 = sum(5)
+#
+# print(f"Total outside function : {number}")
+# print(f"Total  outside function : {number_2}")
+# print(f"Total outside function : {number_3}")
 
-number = sum(5,6)
-number_2 = sum(b=5, a=6)
-number_3 = sum(5)
 
-print(f"Total outside function : {number}")
-print(f"Total  outside function : {number_2}")
-print(f"Total outside function : {number_3}")
+# Exercise: Functions in python
+# 1. Write a function called calculate_area that takes base and height as an input and returns and area of a triangle. Equation of an area of a triangle is,
+# ```
+# area = (1/2)*base*height
+# ```
+#
+# 2. Modify above function to take third parameter shape type. It can be either "triangle" or "rectangle". Based on shape type it will calculate area. Equation of rectangle's area is,
+# ```
+# rectangle area=length*width
+# ```
+# If no shape is supplied then it should take triangle as a default shape
+#
+# 3. Write a function called print_pattern that takes integer number as an argument and prints following pattern if input number is 3,
+# ```
+# *
+# **
+# ***
+# ```
+# if input is 4 then it should print
+# ```
+# *
+# **
+# ***
+# ****
+# ```
+# Basically number of lines it prints is equal to that number. (Hint: you need to use two for loops)
+#
+# [Solution](https://github.com/codebasics/py/blob/master/Basics/Exercise/10_functions/10_functions_exercise.py)
+
+# Kalp Doshi's Solution
+
+def calulate_area(base, height):
+    area = (1/2) * base * height
+    return area
+
+area_of_a_triangle = calulate_area(5, 10)
+print(area_of_a_triangle)
