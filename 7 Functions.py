5b2f19b69799b7112799680aef0072b8991a9d95
kalpd <kalpdoshi999@gmail.com>
2026-07-30 09:34:52 +0530

    Learning Functions

--- 7 Functions.py
+++ 7 Functions.py
@@ -20,17 +20,31 @@
 
 # Using Functions
 
-tom_expense_list = [580, 305, 240, 710]
-joe_expense_list = [320, 670, 180, 760]
+# tom_expense_list = [580, 305, 240, 710]
+# joe_expense_list = [320, 670, 180, 760]
+#
+# def total_expense(exp):
+#     total = 0
+#     for item in exp:
+#         total += item
+#     return total
+#
+# tom_expense = total_expense(tom_expense_list)
+# joe_expense = total_expense(joe_expense_list)
+#
+# print(f"Tom's total expense is : {tom_expense}")
+# print(f"Joe's total expense is : {joe_expense}")
 
-def total_expense(exp):
-    total = 0
-    for item in exp:
-        total += item
+
+def sum(a, b):
+    print(f"A : {a}")
+    print(f"B : {b}")
+    total = a + b
+    print(f"Total inside function : {total}")
     return total
 
-tom_expense = total_expense(tom_expense_list)
-joe_expense = total_expense(joe_expense_list)
+number = sum(5,6)
+number_2 = sum(b=5, a=6)
 
-print(f"Tom's total expense is : {tom_expense}")
-print(f"Joe's total expense is : {joe_expense}")
+print(f"Total outside function : {number}")
+print(f"Total  outside function : {number_2}")
