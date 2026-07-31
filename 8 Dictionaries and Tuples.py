0fbb76ad3a1c52d4eef1c8f5b0a447ee690b36be
kalpd <kalpdoshi999@gmail.com>
2026-07-31 10:44:06 +0530

    Learning Dictionaries and Tuples

--- 8 Dictionaries and Tuples.py
+++ 8 Dictionaries and Tuples.py
@@ -5,3 +5,31 @@ dic = {"tom" : 123456789,
 print(dic)
 
 print(dic["tom"])
+
+dic["sam"] = 9876544521
+
+print(dic)
+
+del dic["sam"]
+
+print(dic)
+
+for key in dic:
+    print(f"Key : {key}, Value : {dic[key]}")
+
+for key, value in dic.items():
+    print(key,value)
+
+print("tom" in dic)
+print("samir" in dic)
+
+dic.clear()
+print(dic)
+
+tuple_point = (5,6)
+
+print(tuple_point)
+print(tuple_point[0])
+print(tuple_point[1])
+
+# tuple_point[0] = 1 We can not change values in tuples.
