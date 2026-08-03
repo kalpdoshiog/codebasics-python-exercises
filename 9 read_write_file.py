b9d264068a43286a105928fdbdfb7053e315b048
kalpd <kalpdoshi999@gmail.com>
2026-08-03 19:53:43 +0530

    Learning Read/Write Files

--- 9 read_write_file.py
+++ 9 read_write_file.py
@@ -1,3 +1,4 @@
 file = open("C:\\Users\\kalpd\\PycharmProjects\\codebasics-python-exercises\\9_read_write_file\\hello.txt","w")
-file.write("I Love Python")
+# file.write("I Love Python")
+file.write("I Love JavaScript")
 file.close()
