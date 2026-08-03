3b8a34b89071ba874482ff505c51f2771a39aa63
kalpd <kalpdoshi999@gmail.com>
2026-08-03 19:59:38 +0530

    Learning Read/Write Files

--- 9 read_write_file.py
+++ 9 read_write_file.py
@@ -1,4 +1,10 @@
-file = open("C:\\Users\\kalpd\\PycharmProjects\\codebasics-python-exercises\\9_read_write_file\\hello.txt","w")
-# file.write("I Love Python")
-file.write("I Love JavaScript") # It will overwrite previous content.
+# file = open("C:\\Users\\kalpd\\PycharmProjects\\codebasics-python-exercises\\9_read_write_file\\hello.txt","w")
+# # file = open("C:\\Users\\kalpd\\PycharmProjects\\codebasics-python-exercises\\9_read_write_file\\hello.txt","a")
+# # file.write("I Love Python")
+# file.write("I Love JavaScript") # It will overwrite previous content.
+# file.write("\nI Love C++") # Now It will append it.
+# file.close()
+
+file = open("C:\\Users\\kalpd\\PycharmProjects\\codebasics-python-exercises\\9_read_write_file\\hello.txt","r")
+print(file.read())
 file.close()
