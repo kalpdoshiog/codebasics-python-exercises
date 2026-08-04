dce47a0940af9772764bac768e6aae1c7f36b477
kalpd <kalpdoshi999@gmail.com>
2026-08-04 10:23:22 +0530

    Learnt Read/Write Files

--- 9 read_write_file.py
+++ 9 read_write_file.py
@@ -54,22 +54,49 @@
 #
 # [Solution](https://github.com/codebasics/py/blob/master/Basics/Exercise/13_read_write_files/exercise_2_stocks.py)
 
-poem_file = open("C:\\Users\\kalpd\\PycharmProjects\\codebasics-python-exercises\\9_Read_Write_File\\poem.txt", "r")
+# poem_file = open("C:\\Users\\kalpd\\PycharmProjects\\codebasics-python-exercises\\9_Read_Write_File\\poem.txt", "r")
+#
+# # print(poem_file.read())
+# max_count = 0
+# max_word = ""
+# count = {}
+# for line in poem_file:
+#     words = line.split()
+#     for word in words:
+#         if word not in count:
+#             count[word] = 1
+#         else:
+#             count[word] += 1
+#         if count[word] > max_count:
+#             max_count = count[word]
+#             max_word = word
+# # print(count)
+# print(f"Max Word is: {max_word} and count is: {max_count}")
+# poem_file.close()
 
-# print(poem_file.read())
-max_count = 0
-max_word = ""
-count = {}
-for line in poem_file:
-    words = line.split()
-    for word in words:
-        if word not in count:
-            count[word] = 1
-        else:
-            count[word] += 1
-        if count[word] > max_count:
-            max_count = count[word]
-            max_word = word
-# print(count)
-print(f"Max Word is: {max_word} and count is: {max_count}")
-poem_file.close()
+stock_file = open("C:\\Users\\kalpd\\PycharmProjects\\codebasics-python-exercises\\9_Read_Write_File\\stocks.csv", "r")
+stock_file_out = open("C:\\Users\\kalpd\\PycharmProjects\\codebasics-python-exercises\\9_Read_Write_File\\stocks_out.csv", "w")
+stock_file.readline()
+
+stock_file_out.write("Company Name,PE Ratio,PB Ratio \n")
+for line in stock_file:
+    words = line.split(",")
+
+    company_name = words[0]
+    price = words[1]
+    earning_per_share = words[2]
+    book_value = words[3]
+
+    price = float(price)
+    earning_per_share = float(earning_per_share)
+    book_value = float(book_value)
+
+    pe_ratio = price/earning_per_share
+    price_to_book = price/ book_value
+
+    pe_ratio = round(pe_ratio, 2)
+    pb_ratio = round(price_to_book, 2)
+    stock_file_out.write(f"{company_name},{pe_ratio},{pb_ratio}\n")
+
+stock_file.close()
+stock_file_out.close()
